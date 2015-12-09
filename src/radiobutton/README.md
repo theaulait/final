bafc405857500f49d5ad47bb6b97e70ef7e32801
Alex Samuel <alex@alexsamuel.net>
2015-12-09 13:46:49 -0500

    Problem 3.

---
+++ src/radiobutton/README.md
@@ -0,0 +1,20 @@
+# Radio Button Groups
+
+In a user interface, a "radio button group" is a group of buttons in which
+exactly one button may be selected at a time. (If you press one button, the
+other button that is currently selected "pops up" like buttons on an 
+old-fashioned radio.)
+
+Your task is to implement a class that models this behavior. The class 
+represents a group of buttons; the number of buttons is specified at
+construction. The class tracks which button is selected.
+
+We've provided a code skeleton in
+[RadioButtonGroup.java](RadioButtonGroup.java). Implement all methods that
+have been provided. Add class attributes and helper methods as needed, but
+do not change any of the provided method signatures.
+
+Your implementation should raise `RuntimeException` on invalid input, as 
+specified in the methods' JavaDoc comments.
+
+Feel free to add a `main()` method to test the logic, if you wish.
---
+++ src/radiobutton/RadioButtonGroup.java
@@ -0,0 +1,58 @@
+package radiobutton;
+
+/**
+ * Represents a group of "radio buttons": toggle buttons for which exactly one is selected
+ * at a given time. If a different button is selected, the previously-selected button is
+ * automatically unselected.
+ *
+ * Buttons are numbered from 0 to one less than the number of buttons. For example, if the
+ * button group has 4 buttons, they are numbered 0, 1, 2, and 3.
+ */
+public class RadioButtonGroup {
+
+    /**
+     * Creates a group of radio buttons.
+     *
+     * @param numButtons
+     *   The number of buttons in the group.
+     * @param initial
+     *   The button number that is initially selected.
+     * @raise RuntimeException
+     *   The initial button number is invalid.
+     */
+    public RadioButtonGroup(int numButtons, int initial) {
+        // TODO: Implement this method.
+    }
+
+    /**
+     * Creates a group of radio buttons. Button 0 is initially selected.
+     */
+    public RadioButtonGroup(int numButtons) {
+        this(numButtons, 0);
+    }
+
+    /**
+     * Selects a button, unselecting the button that was previously selected.
+     * @param button
+     *   The button number to select.
+     * @raise RuntimeException
+     *   The button number is invalid.
+     */
+    public void select(int button) {
+        // TODO: Implement this method.
+    }
+
+    /**
+     * Returns whether a given button is selected..
+     * @param button
+     *   The button number to check.
+     * @raise RuntimeException
+     *   The button number is invalid.
+     */
+    public boolean isSelected(int button) {
+        // TODO: Implement this method.
+    }
+
+    // TODO: Add attributes and helper methods as needed.
+
+}
